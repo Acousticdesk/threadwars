d2e47d32f6a8e91306a12fda8978b43d634ec84a
Andrii Kicha <akicha@star.global>
2022-10-05 21:00:43 +0300

    squash

--- threadwars/main.cpp
+++ threadwars/main.cpp
@@ -1,14 +1,80 @@
-//
-//  main.cpp
-//  threadwars
-//
-//  Created by Andrii Kicha on 05.10.2022.
-//
-
 #include <iostream>
+#include <ncurses.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int width = 50;
+int height = 25;
+
+int gunX = width / 2;
+int gunY = height - 1;
+
+int direction = -1;
+
+void Draw()
+{
+  for (int i = 0; i <= height; i += 1)
+  {
+    for (int j = 0; j <= width; j += 1)
+    {
+        if (i == gunY && j == gunX) {
+          printw("X");
+          continue;
+        }
+ 
+        if (i == 0 || i == height || j == 0 || j == width) {
+          printw("#");
+          continue;
+        }
+
+        printw(" ");
+    }
+      printw("\n");
+  }
+}
+
+void Input()
+{
+    switch(getch()) {
+        case 67:    // key right
+            direction = 1;
+            break;
+        case 68:    // key left
+            direction = 0;
+            break;
+        default:
+            direction = -1;
+    }
+}
+
+void Logic()
+{
+    switch (direction) {
+        case 1:
+            gunX++;
+            break;
+        case 0:
+            gunX--;
+            break;
+        default:
+            break;
+    }
+}
+
+int main() {
+    initscr();
+    noecho();
+    nodelay(stdscr, TRUE);
+    while (true) {
+        // First get input from the user, then claculate the changes, then draw the updates
+        Input();
+        // Erase shall be kept under the Input call. Otherwise flickering happens. This is because
+        // erase should never be called after the wgetch();
+        erase();
+        Logic();
+        Draw();
+        refresh();
+        
+    }
     return 0;
 }
