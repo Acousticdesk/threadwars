dc233418d22a96f00832ec32ac7259e1e6c803b4
Andrii Kicha <akicha@star.global>
2022-10-06 19:52:21 +0300

    squash

--- threadwars/main.cpp
+++ threadwars/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ncurses.h>
+#include <thread>
 
 using namespace std;
 
@@ -11,6 +12,80 @@ int gunY = height - 1;
 
 int direction = -1;
 
+int** bullets = new int*[3];
+
+void CreateBullet() {
+    int bullet[2] = { gunX, gunY };
+    
+    int bulletsOnTheScreen = 0;
+    
+    // check how many bullets do we already have
+    for (int i = 0; i < 3; i++) {
+        if (bullets[i]) {
+            bulletsOnTheScreen++;
+        }
+    }
+    
+    // no need to fire new bullets is the maximum amount was reached
+    if (bulletsOnTheScreen == 3) {
+        return;
+    }
+    
+    // Adding a next bullet to the array (fire the bullet)
+    
+    // max amount of bullets is 3, todo: move it to a constant and initialize during the setup phase
+    int indexOfBullet = -1;
+    
+    for (int i = 0; i < 3; i++ ) {
+        if (!bullets[i]) {
+            indexOfBullet = i;
+            bullets[i] = new int[2];
+            for (int j = 0; j < 2; j++) {
+                bullets[i][j] = bullet[j];
+            }
+        }
+    }
+    
+    bool isBulletOnScreen = true;
+    
+    while ((isBulletOnScreen = bullet[1] > 0)) {
+        bullet[1]--;
+    }
+    
+     bullets[indexOfBullet] = NULL;
+    
+//    int indexOfBullet = -1;
+//
+//    if (bulletsOnTheScreen == 3) {
+//        bullets[0] = new int[2];
+//
+//        for (int j = 0; j < 2; j++) {
+//            bullets[0][j] = bullet[j];
+//        }
+//    } else {
+//        for (int i = 0; i < 3; i++ ) {
+//            if (!bullets[i]) {
+//                bullets[i] = new int[2];
+//                for (int j = 0; j < 2; j++) {
+//                    bullets[i][j] = bullet[j];
+//                }
+//            }
+//        }
+//    }
+    
+//    int numberOfBullets = sizeof(bullets) / sizeof(bullets[0]);
+    
+//    for (int i = 0; i < numberOfBullets; i++) {
+//        if (bullets[i + 1]) {
+//            bullets[i] = bullets[i + 1];
+//        } else {
+//            break;
+//        }
+//    }
+//
+//    bullets[numberOfBullets - 1] = { bulletX, bulletY };
+}
+
 void Draw()
 {
   for (int i = 0; i <= height; i += 1)
@@ -19,12 +94,16 @@ void Draw()
     {
         if (i == gunY && j == gunX) {
           printw("X");
-          continue;
         }
  
         if (i == 0 || i == height || j == 0 || j == width) {
           printw("#");
-          continue;
+        }
+        
+        for (int k = 0; k < 3; k++) {
+            if (bullets[k] && bullets[k][1] == i && bullets[k][0] == j) {
+                printw("x");
+            }
         }
 
         printw(" ");
@@ -42,6 +121,11 @@ void Input()
         case 68:    // key left
             direction = 0;
             break;
+        case 32: {
+            thread trackBullet(CreateBullet);
+            trackBullet.join();
+            return;
+        }
         default:
             direction = -1;
     }
@@ -76,5 +160,6 @@ int main() {
         refresh();
         
     }
+    // endwin();
     return 0;
 }
